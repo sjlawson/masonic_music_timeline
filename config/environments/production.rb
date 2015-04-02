e124a607bfa2d1dbdac9cdf54e7344efbf491ba0
Samuel Lawson <slawson@mooreheadcomm.com>
2015-04-02 11:39:36 -0400

    re-added config.serve_static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # says it's deprecated, but doesn't work without it
+  config.serve_static_assets = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
