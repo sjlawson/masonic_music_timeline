992e2b76b212019e84ea97071b43c50d2236aea6
Samuel Lawson <slawson@mooreheadcomm.com>
2015-04-02 13:26:01 -0400

    visual improvements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   protected
   def require_login
     unless current_user
-      redirect_to root_path
+      redirect_to "/users/sign_in"
     end
   end
 
